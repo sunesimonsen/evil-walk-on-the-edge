2d1f99978be5d2ceeec87f335b96024f79777001
Sune Simonsen <sune@we-knowhow.dk>
2014-03-15 14:41:37 +0100

    Initial commit

---
+++ evil-walk-on-the-edge.el
@@ -0,0 +1,25 @@
+(require 'evil)
+
+(evil-define-motion evil-move-forward-paren (count)
+  "Move forward to next (, [, {, }, ] or )"
+  :jump t
+  :type inclusive
+  (interactive "<c>")
+  (setq count (or count 1))
+  (forward-char)
+  (re-search-forward "\\s(\\|\\s)" nil 'end-of-buffer count)
+  (while (in-string-p)
+    (re-search-forward "\\s(\\|\\s)" nil 'end-of-buffer))
+  (backward-char))
+
+(evil-define-motion evil-move-backward-paren (count)
+  "Move backward to previous (, [, {, }, ] or )"
+  :jump t
+  :type inclusive
+  (interactive "<c>")
+  (setq count (or count 1))
+  (re-search-backward "\\s(\\|\\s)" nil 'beginning-of-buffer count)
+  (while (in-string-p)
+    (re-search-backward "\\s(\\|\\s)")))
+
+(provide 'evil-walk-on-the-edge)
